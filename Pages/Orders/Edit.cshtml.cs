6f1de0cc2d4470c4e04ce8f724eb3e5b741b8314
Viktor <viktor.v.antoniuk@gmail.com>
2021-03-29 13:22:15 +0300

    Status field for Order added

--- Pages/Orders/Edit.cshtml.cs
+++ Pages/Orders/Edit.cshtml.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,12 +12,17 @@ namespace Bakery.RazorPages.Admin.Pages.Orders
     {
         private readonly OrderService _orderService;
 
+        public SelectList Statuses { get; set; }
+
+        public List<string> status = new List<string>() {"NEW", "Processed", "Delivered", "Completed"};
+
         [BindProperty]
         public Order Order { get; set; }
 
         public EditModel(OrderService orderService)
         {
             _orderService = orderService;
+            Statuses = new SelectList(status); 
         }
 
         public async Task OnGet(int id)
